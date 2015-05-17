a881655dea63531c909742baf39a18df45f05c3d
Cory Benfield <lukasaoz@gmail.com>
2015-05-17 11:12:00 +0100

    Install requirements

--- setup.py
+++ setup.py
@@ -20,6 +20,14 @@ setup(
     packages=packages,
     package_dir={'testifi': 'testifi'},
     license='MIT License',
+    install_requires=[
+        'Twisted>=15.1.0',
+        'pyOpenSSL>=0.15.1',
+        'service-identity>=14.0.0',
+        'structlog>=15.1.0',
+        'treq>=15.0.0',
+        'requests>=2.7.0',
+    ],
     entry_points={
         'console_scripts': [
             'testifi = testifi.server:runServer',
